210c8390b48e733e946ed5a623fd35cdbbbf1109
marcelogaldino <marcelinhomgo@gmail.com>
2021-04-25 15:56:10 -0300

    context api, improved player and modified some css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import { api } from '../services/api'
 
 import styles from './home.module.scss'
 import { ConvertDurationToTimeString } from '../utils/ConvertDurationToTimeString'
+import { useContext } from 'react'
+import { PlayerContext } from '../contexts/contextPlayer'
+
 
 type Episodes = {
   id: string;
@@ -25,6 +28,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -51,7 +56,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="./play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
